9621a9a5014984f4780f7f3c90b28575d6c78fa7
Sharnetta <sharnettalamar@yahoo.com>
2020-07-03 17:32:34 -0400

    array-iter

--- main.js
+++ main.js
@@ -7,3 +7,56 @@ console.log(users)
 
 // Append the katas to this element:
 const main = document.querySelector('main')
+
+
+function printKata (kataNumber, object) {
+    // For the usage of the DETAILS and SUMMARY tags
+    // in HTML, see: http://mdn.io/details-element
+    const detailsElement = document.createElement('details')
+    main.append(detailsElement)
+    //
+    const summaryElement = document.createElement('summary')
+    summaryElement.append("KATA " + kataNumber)
+    detailsElement.append(summaryElement)
+    //
+    // http://mdn.io/json.stringify
+    const stringifiedObject = JSON.stringify(object)
+    detailsElement.append(stringifiedObject)
+}
+
+const greenEyes1 = users.filter(user => user.eyeColor === "green")
+printKata(0, greenEyes1) 
+ 
+ 
+ 
+const isActive1 = users.filter(user => user.isActive === true)
+printKata(1, isActive1)
+ 
+const email1 = users.map(user => user.email )
+printKata(2, email1)
+ 
+ 
+const company1 = users.some(user => user.company === 'OVATION')
+printKata(3, company1)
+ 
+ 
+const age1 = users.find(user => user.age < 38)
+printKata(4, age1)
+ 
+const over38Active = users
+       .filter(user => user.age > 38)
+      .find(user => user.isActive)
+printKata(5, over38Active)
+ 
+const userBalance = users
+.filter(user => user.company === 'ZENCO')
+.map(user => user.balance)
+printKata(6, userBalance)
+ 
+ 
+const ageAndTag = users
+    
+      .filter(user => user.tags.includes('fugiat'))
+      .map(user => user.age)
+printKata(7, ageAndTag)
+
